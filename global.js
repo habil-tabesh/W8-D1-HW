55f18838aecee90314396a0251dcb10f3369b536
Habil Tabesh <habil.tabesh134@gmail.com>
2022-05-16 18:38:47 -0400

    done

---
+++ global.js
@@ -0,0 +1,5 @@
+// Global object
+
+console.log(global);
+
+set
---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Habil', 'Spencer', 'Emma', 'Angel'];
+const ages = [22, 30, 19, 20];
+
+console.log(people);
+
+module.exports = {
+    people,
+    ages
+};
